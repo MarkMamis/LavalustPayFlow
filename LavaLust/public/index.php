b03417ed3f0f5ad3a88c828ea7e48d08dd09ee07
MarkMamis <nebresmarkedniel22@gmail.com>
2025-11-28 02:05:51 +0800

    Add public/index.php to bootstrap application for webroot

---
+++ LavaLust/public/index.php
@@ -0,0 +1,4 @@
+<?php
+// Bootstrap the application from the project root index.php
+// This lets Apache serve the app when DocumentRoot is set to LavaLust/public
+require_once __DIR__ . '/../index.php';
